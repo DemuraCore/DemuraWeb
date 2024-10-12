ef800d295c793f6396a8e8e04e1d1459f624e3fc
DemuraAIdev <cyberaioff@gmail.com>
2024-10-12 16:33:19 +0700

    refactor: Update astro.config.mjs
    Update astro.config.mjs to include the site URL, integrations, and server configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://demura.vahry.my.id",
+  integrations: [tailwind()],
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
